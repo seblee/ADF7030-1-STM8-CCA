261daafc50253955db9363ca2464bcf4607e536a
Yisa Lee <ceeu@vip.qq.com>
2017-06-02 12:34:16 +0800

    TestBranch

--- user_src/eeprom.c
+++ user_src/eeprom.c
@@ -7,19 +7,19 @@
 /*  Mark        :STM8S207C8��CODE�ռ�Ϊ64K                             */
 /*              :STM8S207C8��EEPROM�Ĵ�СΪ1536�ֽ�,��:3ҳ,512��/ҳ    */
 /***********************************************************************/
-#include  <iostm8l151g4.h>				// CPU�ͺ�
+#include <iostm8l151g4.h> // CPU�ͺ�
 //#include "stm8l15x.h"
-#include "Pin_define.h"		// �ܽŶ���
+#include "Pin_define.h" // �ܽŶ���
 //#include "initial.h"		// ��ʼ��  Ԥ����
-#include "ram.h"		// RAM����
-#include "eeprom.h"		// eeprom
+#include "ram.h"    // RAM����
+#include "eeprom.h" // eeprom
 #include "ID_Decode.h"
 /***********************************************************************/
 /*                    FLASH & EEPROM �Ĵ���������λ                    */
 /***********************************************************************/
-#define FIRST_SECURITY_KEY        0xAE
-#define SECOND_SECURITY_KEY       0x56
-#define ADD_EEPROM_S8             0x1000
+#define FIRST_SECURITY_KEY 0xAE
+#define SECOND_SECURITY_KEY 0x56
+#define ADD_EEPROM_S8 0x1000
 
 ///* FLASH_CR2 */
 //#define OPT               7   /* ��ѡ���ֽڽ���д���� */
@@ -83,147 +83,160 @@
 //
 /* FLASH_IAPSR */
 //#define NC              7
-#define HVOFF             6   /* ��ѹ������־ */
+#define HVOFF 6 /* ��ѹ������־ */
 //#define NC              5
 //#define NC              4
-#define DUL               3   /* DATA EEPROM����������־ */
-#define EOP               2   /* ���̽���(д����������)��־ */
-#define PUL               1   /* ���ٳ����洢��������־ */
-#define WR_PG_DIS         0   /* ��ͼ�򱻱���ҳ����д�����ı�־ */
+#define DUL 3       /* DATA EEPROM����������־ */
+#define EOP 2       /* ���̽���(д����������)��־ */
+#define PUL 1       /* ���ٳ����洢��������־ */
+#define WR_PG_DIS 0 /* ��ͼ�򱻱���ҳ����д�����ı�־ */
 
-#define FLASH_CR1_RESET_VALUE   (( uchar )0x00 )
-#define FLASH_CR2_RESET_VALUE   (( uchar )0x00 )
-#define FLASH_NCR2_RESET_VALUE  (( uchar )0xFF )
-#define FLASH_IAPSR_RESET_VALUE (( uchar )0x40 )
-#define FLASH_PUKR_RESET_VALUE  (( uchar )0x00 )
-#define FLASH_DUKR_RESET_VALUE  (( uchar )0x00 )
+#define FLASH_CR1_RESET_VALUE ((uchar)0x00)
+#define FLASH_CR2_RESET_VALUE ((uchar)0x00)
+#define FLASH_NCR2_RESET_VALUE ((uchar)0xFF)
+#define FLASH_IAPSR_RESET_VALUE ((uchar)0x40)
+#define FLASH_PUKR_RESET_VALUE ((uchar)0x00)
+#define FLASH_DUKR_RESET_VALUE ((uchar)0x00)
 
-
-
-#define FLASH_PUKR_PUK          ((uchar)0xFF) /*!< Flash Program memory unprotection mask */
-#define FLASH_DUKR_DUK          ((uchar)0xFF) /*!< Data EEPROM unprotection mask */
+#define FLASH_PUKR_PUK ((uchar)0xFF) /*!< Flash Program memory unprotection mask */
+#define FLASH_DUKR_DUK ((uchar)0xFF) /*!< Data EEPROM unprotection mask */
 
 //#define UNLOCK_FLASH_TYPE       (( uchar )0x00 )
 //#define UNLOCK_EEPROM_TYPE      (( uchar )0x01 )
 
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-void InitialFlashReg( void ){					// ��ʼ�������Ĵ�����
-	FLASH_CR1 = FLASH_CR1_RESET_VALUE;
-	FLASH_CR2 = FLASH_CR2_RESET_VALUE;
-	//FLASH_NCR2 = FLASH_NCR2_RESET_VALUE;
-	FLASH_IAPSR &= ( uchar )(~( 1 << DUL ));	// ����ֻ��DATA������
-	FLASH_IAPSR &= ( uchar )(~( 1 << PUL ));	// ��������������
+void InitialFlashReg(void)
+{ // ��ʼ�������Ĵ�����
+    FLASH_CR1 = FLASH_CR1_RESET_VALUE;
+    FLASH_CR2 = FLASH_CR2_RESET_VALUE;
+    //FLASH_NCR2 = FLASH_NCR2_RESET_VALUE;
+    FLASH_IAPSR &= (uchar)(~(1 << DUL)); // ����ֻ��DATA������
+    FLASH_IAPSR &= (uchar)(~(1 << PUL)); // ��������������
 }
 //------------------------------------------------
 //  ע: 2����Կ�Ĳ������������෴
-void UnlockFlash(unsigned char Type){			// ����flash
-	if( Type == UNLOCK_FLASH_TYPE){				// ����������
-		FLASH_DUKR = SECOND_SECURITY_KEY;
-		FLASH_DUKR = FIRST_SECURITY_KEY;
-	}
-	else{										// ����eeprom
-		FLASH_DUKR = FIRST_SECURITY_KEY;
-		FLASH_DUKR = SECOND_SECURITY_KEY;
-	}
+void UnlockFlash(unsigned char Type)
+{ // ����flash
+    if (Type == UNLOCK_FLASH_TYPE)
+    { // ����������
+        FLASH_DUKR = SECOND_SECURITY_KEY;
+        FLASH_DUKR = FIRST_SECURITY_KEY;
+    }
+    else
+    { // ����eeprom
+        FLASH_DUKR = FIRST_SECURITY_KEY;
+        FLASH_DUKR = SECOND_SECURITY_KEY;
+    }
 }
 //------------------------------------------------
-void LockFlash(unsigned char Type ){			// �����洢��
-	if( Type == UNLOCK_FLASH_TYPE ){
-		FLASH_IAPSR &= ~( 1 << PUL );
-	}
-	else{
-		FLASH_IAPSR &= ~( 1 << DUL );
-	}
+void LockFlash(unsigned char Type)
+{ // �����洢��
+    if (Type == UNLOCK_FLASH_TYPE)
+    {
+        FLASH_IAPSR &= ~(1 << PUL);
+    }
+    else
+    {
+        FLASH_IAPSR &= ~(1 << DUL);
+    }
 }
 //------------------------------------------------
-uchar ReadByteEEPROM( ulong Addr ){				// ��eeprom�ж�ȡ1�ֽ�
-	return(*(( __far uchar* ) Addr ));			// Read byte
+uchar ReadByteEEPROM(ulong Addr)
+{                                    // ��eeprom�ж�ȡ1�ֽ�
+    return (*((__far uchar *)Addr)); // Read byte
 }
 //------------------------------------------------
-void WriteByteToFLASH(ulong Addr, uchar Dat){	// д��һ�ֽڵ�eeprom
-	*(( __far uchar * ) Addr ) = Dat;
+void WriteByteToFLASH(ulong Addr, uchar Dat)
+{ // д��һ�ֽڵ�eeprom
+    *((__far uchar *)Addr) = Dat;
 }
 //------------------------------------------------
-void EraseByteFLASH( uint Addr){				// ����eeprom������
-	*(( __near uchar * ) Addr) = 0x00;
+void EraseByteFLASH(uint Addr)
+{ // ����eeprom������
+    *((__near uchar *)Addr) = 0x00;
 }
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-void eeprom_save(void){
-	unsigned char	i,eeprom_sys_buff;
-	
-	UnlockFlash( UNLOCK_EEPROM_TYPE );
-	for (i=0;i<16;i++)
-		WriteByteToFLASH( addr_eeprom_sys+i, eeprom_sys_buff);
-	LockFlash( UNLOCK_EEPROM_TYPE );
+void eeprom_save(void)
+{
+    unsigned char i, eeprom_sys_buff;
+
+    UnlockFlash(UNLOCK_EEPROM_TYPE);
+    for (i = 0; i < 16; i++)
+        WriteByteToFLASH(addr_eeprom_sys + i, eeprom_sys_buff);
+    LockFlash(UNLOCK_EEPROM_TYPE);
 }
-void eeprom_sys_load(void){
-    UINT16 i,j,q,p;
-    UINT8 xm[3]={0};
+void eeprom_sys_load(void)
+{
+    UINT16 i, j, q, p;
+    UINT8 xm[3] = {0};
     uni_rom_id xn;
 
-
-    for(i = 0;i < 256;i++)ID_Receiver_DATA[i] = 0;
-    xm[0] = ReadByteEEPROM(addr_eeprom_sys + 0x3FE );
-    xm[1] = ReadByteEEPROM(addr_eeprom_sys + 0x3FF );
+    for (i = 0; i < 256; i++)
+        ID_Receiver_DATA[i] = 0;
+    xm[0] = ReadByteEEPROM(addr_eeprom_sys + 0x3FE);
+    xm[1] = ReadByteEEPROM(addr_eeprom_sys + 0x3FF);
     ID_DATA_PCS = xm[0] * 256 + xm[1];
-    if(ID_DATA_PCS == 0xFFFF)ID_DATA_PCS = 0;
-    else if(ID_DATA_PCS > 256)ID_DATA_PCS = 256;
+    if (ID_DATA_PCS == 0xFFFF)
+        ID_DATA_PCS = 0;
+    else if (ID_DATA_PCS > 256)
+        ID_DATA_PCS = 256;
     q = ID_DATA_PCS;
     p = 0;
-    for(i=0;i<q;i++)
+    for (i = 0; i < q; i++)
     {
         j = 3 * i;
         xm[0] = ReadByteEEPROM(addr_eeprom_sys + j);
-	    j++;
+        j++;
         xm[1] = ReadByteEEPROM(addr_eeprom_sys + j);
-	    j++;
-        xm[2] = ReadByteEEPROM(addr_eeprom_sys + j);	
+        j++;
+        xm[2] = ReadByteEEPROM(addr_eeprom_sys + j);
         xn.IDB[0] = 0;
         xn.IDB[1] = xm[0];
         xn.IDB[2] = xm[1];
         xn.IDB[3] = xm[2];
-        if((xn.IDL == 0)||(xn.IDL == 0xFFFFFF))q++;
-        else ID_Receiver_DATA[p++] = xn.IDL;
-        if(q > 260)break;
+        if ((xn.IDL == 0) || (xn.IDL == 0xFFFFFF))
+            q++;
+        else
+            ID_Receiver_DATA[p++] = xn.IDL;
+        if (q > 260)
+            break;
         ClearWDT(); // Service the WDT
     }
 
-
-  for (i=1;i<2;i++)
-  {
-    j=0x380+i*4;
-//    ROM_adf7012_value[i].byte[0]= ReadByteEEPROM( addr_eeprom_sys+j );
-//    ROM_adf7012_value[i].byte[1]= ReadByteEEPROM( addr_eeprom_sys+j+1 );
-//    ROM_adf7012_value[i].byte[2]= ReadByteEEPROM( addr_eeprom_sys+j+2 );
-//    ROM_adf7012_value[i].byte[3]= ReadByteEEPROM( addr_eeprom_sys+j+3 );
-//    if((ROM_adf7012_value[i].whole_reg==0)||(ROM_adf7012_value[i].whole_reg==0xFFFFFFFF))ROM_adf7012_value[i]=Default_adf7012_value[i];
-  }
-
-
+    for (i = 1; i < 2; i++)
+    {
+        j = 0x380 + i * 4;
+        //    ROM_adf7012_value[i].byte[0]= ReadByteEEPROM( addr_eeprom_sys+j );
+        //    ROM_adf7012_value[i].byte[1]= ReadByteEEPROM( addr_eeprom_sys+j+1 );
+        //    ROM_adf7012_value[i].byte[2]= ReadByteEEPROM( addr_eeprom_sys+j+2 );
+        //    ROM_adf7012_value[i].byte[3]= ReadByteEEPROM( addr_eeprom_sys+j+3 );
+        //    if((ROM_adf7012_value[i].whole_reg==0)||(ROM_adf7012_value[i].whole_reg==0xFFFFFFFF))ROM_adf7012_value[i]=Default_adf7012_value[i];
+    }
 }
 
 void ALL_ID_EEPROM_Erase(void)
 {
-  UINT16 m2,i;
-  UINT8 xm[3]={0};
-       xm[0]=0;
-       xm[1]=0;
-       xm[2]=0;
-       for(i=0;i<260;i++){
-           m2=3*i;
-           UnlockFlash( UNLOCK_EEPROM_TYPE );
-	   WriteByteToFLASH( addr_eeprom_sys+m2, xm[0]);
-	   m2++;
-	   WriteByteToFLASH( addr_eeprom_sys+m2, xm[1]);
-	   m2++;
-	   WriteByteToFLASH( addr_eeprom_sys+m2, xm[2]);
-           LockFlash( UNLOCK_EEPROM_TYPE );
-           ClearWDT(); // Service the WDT
-       }
+    UINT16 m2, i;
+    UINT8 xm[3] = {0};
+    xm[0] = 0;
+    xm[1] = 0;
+    xm[2] = 0;
+    for (i = 0; i < 260; i++)
+    {
+        m2 = 3 * i;
+        UnlockFlash(UNLOCK_EEPROM_TYPE);
+        WriteByteToFLASH(addr_eeprom_sys + m2, xm[0]);
+        m2++;
+        WriteByteToFLASH(addr_eeprom_sys + m2, xm[1]);
+        m2++;
+        WriteByteToFLASH(addr_eeprom_sys + m2, xm[2]);
+        LockFlash(UNLOCK_EEPROM_TYPE);
+        ClearWDT(); // Service the WDT
+    }
 }
 void ID_EEPROM_write(void)
 {
-/*    UINT8 xm[3]={0};
+    /*    UINT8 xm[3]={0};
     UINT16 i,j,m1;
     uni_rom_id xn,xd;
      ID_DATA_PCS++;
@@ -269,10 +282,9 @@ void ID_EEPROM_write(void)
   */
 }
 
-
 void ID_EEPROM_write_0x00(void)
 {
-/*   UINT8 xm[3]={0};
+    /*   UINT8 xm[3]={0};
    UINT16 i,j,m1,q,p;
    uni_rom_id xn,xd;
 
@@ -333,17 +345,15 @@ void ID_EEPROM_write_0x00(void)
     */
 }
 
-
 void SW2_SW3_KEY(void)
 {
-//   if(HA_ERR_signal==0){   //SW3
-//      TIME_key_SW3++;
-//      if((TIME_key_SW3>=3)&&(Flag_key_SW3==0)){
-//        Flag_key_SW3=1;
-//        Count_key_SW3++;
-//        if(Count_key_SW3>1)Count_key_SW3=0;
-//      }
-//   }
-//   else {TIME_key_SW3=0;Flag_key_SW3=0;}
+    //   if(HA_ERR_signal==0){   //SW3
+    //      TIME_key_SW3++;
+    //      if((TIME_key_SW3>=3)&&(Flag_key_SW3==0)){
+    //        Flag_key_SW3=1;
+    //        Count_key_SW3++;
+    //        if(Count_key_SW3>1)Count_key_SW3=0;
+    //      }
+    //   }
+    //   else {TIME_key_SW3=0;Flag_key_SW3=0;}
 }
-
--- user_src/ram.h
+++ user_src/ram.h
@@ -3,49 +3,48 @@
 //CPU?????
 //?????ADF030???????
 //#define TX_TEST_CARRIER_MODE 0
-#include "initial.h"		// 初始化  预定义
+#include "initial.h" // 初始化  预定义
 
-
-extern volatile union{
+extern volatile union {
 	unsigned char BYTE;
-	struct {
-		unsigned char	Bit0:	1;
-		unsigned char	Bit1:	1;
-		unsigned char	Bit2:	1;
-		unsigned char	Bit3:	1;
-		unsigned char	Bit4:	1;
-		unsigned char	Bit5:	1;
-		unsigned char	Bit6:	1;
-		unsigned char	Bit7:	1;
-	}BIT;
-}FLAG_test1;
-	//************************************************
-	#define 	FLAG_test1_BYTE		FLAG_test1.BYTE
-	//------------------------------------------------
-    #define		BIT_SIO          	FLAG_test1.BIT.Bit0
-    #define		FG_auto_out	        FLAG_test1.BIT.Bit1
-    #define		FG_OUT_OPEN_CLOSE	FLAG_test1.BIT.Bit2    //历史记录   误码率测试用
-//    #define		FG_auto_outbz		FLAG_test1.BIT.Bit3
-    #define		FG_auto_open_time	FLAG_test1.BIT.Bit4
-    #define		FG_auto_manual_mode	FLAG_test1.BIT.Bit5      //1=auto,0=manual
-    #define		FG_Receiver_LED_RX	FLAG_test1.BIT.Bit6
-    #define		FG_First_auto		FLAG_test1.BIT.Bit7
-	//************************************************
+	struct
+	{
+		unsigned char Bit0 : 1;
+		unsigned char Bit1 : 1;
+		unsigned char Bit2 : 1;
+		unsigned char Bit3 : 1;
+		unsigned char Bit4 : 1;
+		unsigned char Bit5 : 1;
+		unsigned char Bit6 : 1;
+		unsigned char Bit7 : 1;
+	} BIT;
+} FLAG_test1;
+//************************************************
+#define FLAG_test1_BYTE FLAG_test1.BYTE
+//------------------------------------------------
+#define BIT_SIO FLAG_test1.BIT.Bit0
+#define FG_auto_out FLAG_test1.BIT.Bit1
+#define FG_OUT_OPEN_CLOSE FLAG_test1.BIT.Bit2 //历史记录   误码率测试用
+											  //    #define		FG_auto_outbz		FLAG_test1.BIT.Bit3
+#define FG_auto_open_time FLAG_test1.BIT.Bit4
+#define FG_auto_manual_mode FLAG_test1.BIT.Bit5 //1=auto,0=manual
+#define FG_Receiver_LED_RX FLAG_test1.BIT.Bit6
+#define FG_First_auto FLAG_test1.BIT.Bit7
+//************************************************
 
 extern UINT8 SIO_cnt;
 extern UINT8 SIO_buff[16];
 extern UINT8 SIO_DATA[16];
 
-
-extern UINT8  TIME_10ms;
+extern UINT8 TIME_10ms;
 extern UINT16 TIMER1s;
 extern UINT16 TIMER300ms;
 extern UINT16 TIMER18ms;
-extern UINT8  TIMER250ms_STOP;
+extern UINT8 TIMER250ms_STOP;
 extern UINT16 TIME_auto_out;
 extern UINT16 TIME_auto_close;
 extern UINT16 time_3sec;
-extern UINT32 ID_Receiver_DATA[256];//写入EEPROM ID的数据
+extern UINT32 ID_Receiver_DATA[256]; //写入EEPROM ID的数据
 extern UINT16 ID_DATA_PCS;
 extern u8 Count_key_SW3;
 extern u8 Display_key_SW3;
@@ -53,25 +52,24 @@ extern u8 Display_key_SW3;
 /**********LCD******************/
 extern uFLAG FlagLCDUpdate;
 
-#define LCDUpdateIDFlag             FlagLCDUpdate.BIT.Bit0
-#define LCDUpdateRISSFlag           FlagLCDUpdate.BIT.Bit1
-#define LCDFirstDisplayFlag         FlagLCDUpdate.BIT.Bit2
+#define LCDUpdateIDFlag FlagLCDUpdate.BIT.Bit0
+#define LCDUpdateRISSFlag FlagLCDUpdate.BIT.Bit1
+#define LCDFirstDisplayFlag FlagLCDUpdate.BIT.Bit2
 /**********RSSI*********************/
 extern u16 LCDRSSI;
 extern u16 RAM_RSSI_SUM;
 extern u8 RAM_RSSI_CNT;
 extern u8 RAM_RSSI_AVG;
 
-
-#define SPI_SEND_BUFF_LONG  	300
-#define SPI_REV_BUFF_LONG  		40
+#define SPI_SEND_BUFF_LONG 300
+#define SPI_REV_BUFF_LONG 40
 
 extern const u8 Radio_Memory_Configuration[];
 #define PROFILE_LONG 264
 extern const UINT8 CONST_DATA_PROFILE_200002E4[PROFILE_LONG];
 #define OPEN_LONG 12
-extern const u8  CONST_OPEN_20000AF0[OPEN_LONG];
-extern UINT8  CONST_TXPACKET_DATA_20000AF0[OPEN_LONG];
+extern const u8 CONST_OPEN_20000AF0[OPEN_LONG];
+extern UINT8 CONST_TXPACKET_DATA_20000AF0[OPEN_LONG];
 #define GENRIC_LONG 108
 extern const UINT8 CONST_GENRIC_PACKET_200004F4[GENRIC_LONG];
 #define DIGFILTLUTS_LONG 32
@@ -108,15 +106,11 @@ extern u32 RADIO_DIG_TX_CFG0_32bit_20000304;
 extern const u8 CONST_AFC_Configuration_400041F8[4];
 extern u16 PA_POWER_OUT[2][18];
 void SPI_INIT(void);
-UINT32 ADF7030_GET_MISC_FW(void);//??MISC_FW?????
+UINT32 ADF7030_GET_MISC_FW(void); //??MISC_FW?????
 void ADF7030_WRITING_PROFILE_FROM_POWERON(void);
 void ADF7030_TRANSMITTING_FROM_POWEROFF(void);
 void ADF7030_RECEIVING_FROM_POWEROFF(void);
 void SCAN_RECEIVE_PACKET(void);
 u32 ConfigurationLen(void);
 
-
-
 #endif
-
-
